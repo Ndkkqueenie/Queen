b43ef8b6b3d68f1dd84b63d8ba97c7223b365bf5
Nduka O. Queendalin <Ndkkqueenie@users.noreply.github.com>
2020-05-27 20:42:15 +0100

    update

---
+++ dist/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("#mycarousel").carousel( { interval: 3000 } );
+   $("#carouselButton").click(function(){
+      if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+          $("#mycarousel").carousel('pause');
+          $("#carouselButton").children("span").removeClass('fa-pause');
+          $("#carouselButton").children("span").addClass('fa-play');
+      }
+      else if ($("#carouselButton").children("span").hasClass('fa-play')){
+          $("#mycarousel").carousel('cycle');
+          $("#carouselButton").children("span").removeClass('fa-play');
+          $("#carouselButton").children("span").addClass('fa-pause');                    
+      }
+  });
+});
